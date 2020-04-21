6ebd8e14de516e946f5cbb5c9daad79205fe6018
Chung Yu <chungyu710@gmail.com>
2020-04-20 22:20:20 -0400

    added eating pellets

--- pellet.py
+++ pellet.py
@@ -5,8 +5,13 @@ class Pellet:
         self.x = x
         self.y = y
         self.screen = screen
+        self.isEaten = False
 
     def draw(self):
         pygame.draw.circle(self.screen, (255, 255, 255), [self.x, self.y], 10)
 
-    # def delete(self):
+    def checkCollision(self, pacmanPosX, pacmanPosY):
+        distSquared = (self.x - pacmanPosX)**2 + (self.y - pacmanPosY)**2
+
+        if distSquared <= (10 + 30)**2:
+            self.isEaten = True
