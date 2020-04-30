e20277afa07b473fa727390b63c95944d8bf8a5a
Chung Yu <chungyu710@gmail.com>
2020-04-30 18:36:56 -0400

    added moveQueue and fixed colourmap dimensions

--- player.py
+++ player.py
@@ -24,6 +24,8 @@ class Player:
                     pygame.image.load(self.folder+"death10.png"),
                     pygame.image.load(self.folder+"death11.png")]
 
+        self.movemap = image.load('movemap.png')
+
         # define screen dimensions
         self.scrnW = width
         self.scrnH = height
@@ -31,12 +33,15 @@ class Player:
         # use an image as reference for pacman dimensions
         self.scale = 25
         self.sprite = pygame.transform.scale(self.imgs_alive[1], (self.scale, self.scale))
-        self.rad = self.sprite.get_width() / 2
+        self.rad = (self.sprite.get_width() / 2) + 1
         self.width = self.sprite.get_width()
         self.height = self.sprite.get_height()
 
+        # define pacman's spawn location
+        self.spawnX = self.scrnW/2 - self.rad + 10
+        self.spawnY = self.scrnH/2 - self.rad + 64
+
         # state variables
-        self.idle = True
         self.isLiving = True
         self.pauseDone = False
 
@@ -47,107 +52,117 @@ class Player:
 
         # current movement variables
         self.vel = 2.5
-        self.x = self.scrnW/2 - self.rad + 10
-        self.y = self.scrnH/2 - self.rad + 60
+        self.x = self.spawnX
+        self.y = self.spawnY
         self.dirX = 0
         self.dirY = 0
 
         # previous movement variables
-        self.prevX = -1
-        self.prevY = -1
-        self.prevDirX = 0
-        self.prevDirY = 0
+        self.prevX = None       #{ initialized as None, since there is no initial previous move
+        self.prevY = None       #{
+        self.prevDirX = 0       #{ stores the direction from the previous frame
+        self.prevDirY = 0       #{
 
         # future movement variables
-        self.moveQueue = 0
-            # NONE  == 0
-            # UP    == 1
-            # DOWN  == 2
-            # LEFT  == 3
-            # RIGHT == 4
+        self.intendedDirX = None    #{ initialized as None, since there is no initial intended move
+        self.intendedDirY = None    #{
 
         # counter variables
         self.lives = 3
         self.score = 0
 
     def move(self):
-        movemap = image.load('movemap.png')
-
-        # store current position for future reference
+        # store current position and direction for future reference
         self.prevX = self.x
         self.prevY = self.y
+        self.prevDirX = self.dirX
+        self.prevDirY = self.dirY
 
-        #this is approximately where packman's sprite will collide with a wall
-        nextX = self.x + self.dirX * self.vel + (self.width/2 * self.dirX)
-        nextY = self.y + self.dirY *self.vel + (self.height/2 * self.dirY)
-
-        if(nextX >= movemap.get_width()):
-           nextX = movemap.get_width() - 1
-
-        if(nextX < 0):
-           nextX = 0
-
-        if(nextY >= movemap.get_height()):
-           nextY = movemap.get_height() - 1
-
-        if(nextY < 0):
-           nextY = 0
-
-
-
-        if movemap.get_at((int(nextX), int(nextY))) != Color(255,255,255):
+        if self.isMoveValid(self.dirX, self.dirY):
             self.x += self.dirX * self.vel
             self.y += self.dirY * self.vel
-            self.moveQueue = 0
-
-
-        #player position is the center of the sprite
-        #this keeps pacman from going off screen
-        if self.x > self.scrnW - self.width/2:
-            self.x = self.scrnW - self.width/2
-        elif self.x < 0 + self.width/2:
-            self.x = 0 + self.width/2
-        if self.y > self.scrnH - self.height/2:
-            self.y = self.scrnH - self.height/2
-        elif self.y < 0 + self.height/2:
-            self.y = 0  + self.height/2
 
     def hasMoved(self):
-        if(self.x == self.prevX and self.y == self.prevY):
+        if self.x == self.prevX and self.y == self.prevY:
             return False
         else:
             return True
 
+    def isMoveValid(self, dirX, dirY):
+        # error handling
+        if dirX == None or dirY == None:
+            return False
+
+        # this is approximately where pacman's sprite will collide with a wall
+        nextX = self.x + (dirX * self.vel)
+        nextY = self.y + (dirY * self.vel)
+
+        # out of bounds error handling for the tunnel
+        if(nextX >= self.movemap.get_width()):
+           nextX = self.movemap.get_width() - 1
+
+        if(nextX < 0):
+           nextX = 0
+
+        if(nextY >= self.movemap.get_height()):
+           nextY = self.movemap.get_height() - 1
+
+        if(nextY < 0):
+           nextY = 0
+
+        # prevents pacman clipping with the wall
+        buffer = 5
+
+        # if going down, check bottom left and bottom right corners of pacman
+        if (dirX == 0 and dirY > 0 and
+            self.movemap.get_at((int(nextX + (self.rad - buffer)), int(nextY + (self.rad - buffer)))) == Color(0, 0, 0) and
+            self.movemap.get_at((int(nextX - (self.rad - buffer)), int(nextY + (self.rad - buffer)))) == Color(0, 0, 0)):
+            return True
+
+        # if going up, check top left and top right corners of pacman
+        elif (dirX == 0 and dirY < 0 and
+            self.movemap.get_at((int(nextX + (self.rad - buffer)), int(nextY - (self.rad - buffer)))) == Color(0, 0, 0) and
+            self.movemap.get_at((int(nextX - (self.rad - buffer)), int(nextY - (self.rad - buffer)))) == Color(0, 0, 0)):
+            return True
+
+        # if going right, check top right and bottom right corners of pacman
+        elif(dirX > 0 and dirY == 0 and
+            self.movemap.get_at((int(nextX + (self.rad - buffer)), int(nextY + (self.rad - buffer)))) == Color(0, 0, 0) and
+            self.movemap.get_at((int(nextX + (self.rad - buffer)), int(nextY - (self.rad - buffer)))) == Color(0, 0, 0)):
+            return True
+
+        # if going left, check top left and bottom left corners of pacman
+        elif(dirX < 0 and dirY == 0 and
+            self.movemap.get_at((int(nextX - (self.rad - buffer)), int(nextY + (self.rad - buffer)))) == Color(0, 0, 0) and
+            self.movemap.get_at((int(nextX - (self.rad - buffer)), int(nextY - (self.rad - buffer)))) == Color(0, 0, 0)):
+            return True
+
+        else:
+            return False
+
     def draw(self, screen):
         if self.isLiving == True:
-            # change state of pacman
-            if self.dirX != 0 or self.dirY != 0:
-                self.idle = False
-            else:
-                self.idle = True
-
             # choose which frame to use
-            if self.idle:
+            # all pacman images face left, so various transformations must be applied depending on pacman's direction
+            if self.dirX > 0:
+                sprite = pygame.transform.flip(self.imgs_alive[self.frame_alive], True, False)
+                scaled = pygame.transform.scale(sprite, (self.scale, self.scale))
+                screen.blit(scaled, (self.x - self.rad, self.y - self.rad))
+            elif self.dirX < 0:
+                sprite = pygame.transform.scale(self.imgs_alive[self.frame_alive], (self.scale, self.scale))
+                screen.blit(sprite, (self.x - self.rad, self.y - self.rad))
+            elif self.dirY > 0:
+                sprite = pygame.transform.rotate(self.imgs_alive[self.frame_alive], 90)
+                scaled = pygame.transform.scale(sprite, (self.scale, self.scale))
+                screen.blit(scaled, (self.x - self.rad, self.y - self.rad))
+            elif self.dirY < 0:
+                sprite = pygame.transform.rotate(self.imgs_alive[self.frame_alive], -90)
+                scaled = pygame.transform.scale(sprite, (self.scale, self.scale))
+                screen.blit(scaled, (self.x - self.rad, self.y - self.rad))
+            else:
+                # the case where pacman is idle
                 sprite = pygame.transform.scale(self.imgs_alive[0], (self.scale, self.scale))
                 screen.blit(sprite, (self.x - self.rad, self.y - self.rad))
-            # if moving
-            else:
-                # all pacman images face left, so various transformations must be applied depending on pacman's direction
-                if self.dirX > 0:
-                    sprite = pygame.transform.flip(self.imgs_alive[self.frame_alive], True, False)
-                    scaled = pygame.transform.scale(sprite, (self.scale, self.scale))
-                    screen.blit(scaled, (self.x - self.rad, self.y - self.rad))
-                elif self.dirX < 0:
-                    sprite = pygame.transform.scale(self.imgs_alive[self.frame_alive], (self.scale, self.scale))
-                    screen.blit(sprite, (self.x - self.rad, self.y - self.rad))
-                elif self.dirY > 0:
-                    sprite = pygame.transform.rotate(self.imgs_alive[self.frame_alive], 90)
-                    scaled = pygame.transform.scale(sprite, (self.scale, self.scale))
-                    screen.blit(scaled, (self.x - self.rad, self.y - self.rad))
-                elif self.dirY < 0:
-                    sprite = pygame.transform.rotate(self.imgs_alive[self.frame_alive], -90)
-                    scaled = pygame.transform.scale(sprite, (self.scale, self.scale))
-                    screen.blit(scaled, (self.x - self.rad, self.y - self.rad))
 
         else:
             # the case where self.isLiving == False and self.pauseDone == False
@@ -168,6 +183,9 @@ class Player:
                     sprite = pygame.transform.rotate(self.imgs_alive[self.frame_alive], -90)
                     scaled = pygame.transform.scale(sprite, (self.scale, self.scale))
                     screen.blit(scaled, (self.x - self.rad, self.y - self.rad))
+                else:
+                    sprite = pygame.transform.scale(self.imgs_alive[0], (self.scale, self.scale))
+                    screen.blit(sprite, (self.x - self.rad, self.y - self.rad))
 
             # the case where self.isLiving == False and self.pauseDone == True
             else:
@@ -176,13 +194,7 @@ class Player:
 
                 # after death animation is done
                 if self.frame_dead == len(self.imgs_dead) - 1:
-                    # respawn pacman
-                    self.isLiving = True
-                    self.frame_alive = 0
-                    self.frame_dead = 0
-                    self.x = self.scrnW/2 - self.rad + 10
-                    self.y = self.scrnH/2 - self.rad + 60
-                    self.pauseDone = False
+                    self.respawnEvents()
 
     def findNode(self, nodes):
         for row in nodes:
@@ -197,6 +209,7 @@ class Player:
 
     def deathEvents(self):
         if self.isLiving == True:
+            self.isLiving = False
             self.dirX = 0
             self.dirY = 0
 
@@ -208,4 +221,21 @@ class Player:
                 # display game over text
                 print("GAME OVER")
 
-            self.isLiving = False
+    def respawnEvents(self):
+        # respawn pacman
+        self.isLiving = True
+        self.pauseDone = False
+
+        self.frame_alive = 0
+        self.frame_dead = 0
+        self.x = self.spawnX
+        self.y = self.spawnY
+
+        self.dirX = 0
+        self.dirY = 0
+        self.prevX = None
+        self.prevY = None
+        self.prevDirX = 0
+        self.prevDirY = 0
+        self.intendedDirX = None
+        self.intendedDirY = None
